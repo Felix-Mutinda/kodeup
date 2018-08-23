f0fabb499d30a948c0c812a90727845936ffb76d
unknown <1mutindafelix@gmail.com>
2018-08-23 16:58:54 +0300

    migrate users > remove plain text password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_133743) do
+ActiveRecord::Schema.define(version: 2018_08_23_135746) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2018_08_23_133743) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.string "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
