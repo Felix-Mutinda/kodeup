f66582c10c9bd22e906b2124cb3b208980f3e81e
unknown <1mutindafelix@gmail.com>
2018-08-23 09:45:36 +0300

    add user model validity tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new email: "3mutindafelix@gmail.com", password: "pass", username: "Third-Felix"
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
 end
