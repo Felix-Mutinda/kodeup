c119c18b654abce347977a2c04b6885fbe50c093
unknown <1mutindafelix@gmail.com>
2018-08-24 15:39:23 +0300

    Style show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,6 @@ class PostsController < ApplicationController
     
     def index
         @posts = Post.all
-        if User.find_by(id: session[:current_user])
-            render 'welcome/index'
-        else
-            redirect_to 'welcom/login'
-        end
     end
     
     def new
